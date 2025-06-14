a7d478859bf350f926a7dc528c1005595a42cb86
pedoc <pedoc@qq.com>
2025-06-14 17:08:21 +0800

    fix: Add native Launcher DLL and support loading it for preloading processes

--- X410Launcher/Tools/Launcher.cs
+++ X410Launcher/Tools/Launcher.cs
@@ -1,20 +1,21 @@
 ﻿using MemoryModule;
+using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 
 namespace X410Launcher.Tools;
 
 public static class Launcher
 {
-    private static readonly NativeAssembly? _launcher;
-
     private delegate bool StartProcessPreloadedDelegate(
         [MarshalAs(UnmanagedType.LPWStr)] string applicationName,
         [MarshalAs(UnmanagedType.LPWStr)] string dllName
     );
-    private static readonly StartProcessPreloadedDelegate? StartProcessPreloaded;
 
+    private static readonly StartProcessPreloadedDelegate? StartProcessPreloaded;
+#if LAUNCHER_FROM_MEMORY
     static Launcher()
     {
         using var launcherStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(
@@ -23,7 +24,7 @@ public static class Launcher
 
         if (launcherStream is not null)
         {
-            _launcher = NativeAssembly.Load(launcherStream, "Launcher.dll");
+            var _launcher = NativeAssembly.Load(launcherStream, "Launcher.dll");
             if (_launcher is not null)
             {
                 StartProcessPreloaded =
@@ -31,6 +32,17 @@ public static class Launcher
             }
         }
     }
+#else
+    static Launcher()
+    {
+        var dll = $"Launcher.{RuntimeInformation.ProcessArchitecture}.dll";
+        var launcher = NativeLibrary.Load(dll);
+        Debug.Assert(launcher != IntPtr.Zero);
+        IntPtr procAddress = NativeLibrary.GetExport(launcher, "StartProcessPreloadedW");
+        StartProcessPreloaded = Marshal.GetDelegateForFunctionPointer<StartProcessPreloadedDelegate>(procAddress);
+        Debug.Assert(StartProcessPreloaded != null);
+    }
+#endif
 
     public static void Launch(string path)
     {
