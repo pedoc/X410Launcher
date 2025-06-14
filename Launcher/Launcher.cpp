1f299092babe87a25caa6700df042977c56239ad
pedoc <pedoc@qq.com>
2025-06-14 14:34:24 +0800

    feat: Add Launcher project(no use mingw)

---
+++ Launcher/Launcher.cpp
@@ -0,0 +1,52 @@
+#include <Windows.h>
+
+#include <detours/detours.h>
+
+#include <algorithm>
+#include <iterator>
+#include <string>
+
+extern "C"
+__declspec(dllexport)
+BOOL
+StartProcessPreloadedW(
+    LPCWSTR lpApplicationName,
+    LPCWSTR lpDllName
+)
+{
+    STARTUPINFOW si = {};
+    si.cb = sizeof(si);
+    PROCESS_INFORMATION pi;
+
+    std::wstring dllNameW = lpDllName;
+    std::string dllName;
+    dllName.reserve(dllNameW.size());
+    std::transform(
+        dllNameW.begin(), dllNameW.end(),
+        std::back_inserter(dllName),
+        [](auto ch) { return (char)ch; }
+    );
+
+    BOOL result = DetourCreateProcessWithDllW(
+        lpApplicationName,
+        NULL,
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi,
+        dllName.c_str(),
+        NULL
+    );
+
+    if (result)
+    {
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+
+    return result;
+}
