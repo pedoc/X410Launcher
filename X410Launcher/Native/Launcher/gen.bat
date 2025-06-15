dbdb95286fb8b5b4860d1caf2ec3cdca442182d3
pedoc <pedoc@qq.com>
2025-06-15 11:05:58 +0800

    fix: Switch build script to use Visual Studio 2022 generator instead of Clang

--- X410Launcher/Native/Launcher/gen.bat
+++ X410Launcher/Native/Launcher/gen.bat
@@ -1,1 +1,2 @@
-cmake -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+@REM cmake -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+cmake -S . -B build -G "Visual Studio 17 2022"
